817e2a107d608450695e7e8ec54751ec0392a5e5
SanduniIshara <sanduniishara1@outlook.com>
2024-07-22 18:52:27 +0530

    initial commit_02

---
+++ todo-next-app/Components/Todo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Todo = () => {
+  return (
+    <tr className="bg-white border-b">
+      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+        1
+      </th>
+      <td className="px-6 py-4">
+        Study
+      </td>
+      <td className="px-6 py-4">
+        Learn Next.js
+      </td>
+      <td className="px-6 py-4">
+        Pending
+      </td>
+      <td className="px-6 py-4 flex gap-1">
+        <button className="py-2 px-4 bg-red-400 text-white rounded-md">Delete</button>
+        <button className="py-2 px-4 bg-blue-400 text-white rounded-md">Done</button>
+      </td>
+    </tr>
+  );
+};
+
+export default Todo;
